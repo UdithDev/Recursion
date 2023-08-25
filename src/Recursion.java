18183aa71e3c4ebff1aeeebe42dbbc303850dcb5
Udith_Dev_Wee <daveudith@gmail.com>
2023-08-25 20:28:44 +0530

    final commit

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,6 +1,5 @@
 public class Recursion {
-
-    static int factorial(int n){
+   static int factorial(int n){
         System.out.println("n: "+n);
         if(n<=1)
             return 1;
@@ -10,4 +9,17 @@ public class Recursion {
         int x = factorial(5);
         System.out.println("X : "+x);
     }
+
+
+    //This is same method by using while loop
+/*
+   static int factorial(int n){
+        int answer=n;
+        while (n>1){
+            answer*=n-1;
+            n--;
+
+        }
+        return answer;
+    }*/
 }
